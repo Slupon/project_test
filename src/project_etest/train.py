98f4ea33acfc21cfe09d98bd63b6a1e54afbd562
Slupon <sergi.luponsantacana@gmail.com>
2026-01-10 13:39:46 +0100

    Initialization of cookiecutter

---
+++ src/project_etest/train.py
@@ -0,0 +1,10 @@
+from project_etest.model import Model
+from project_etest.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
