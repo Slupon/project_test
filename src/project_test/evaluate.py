79443b972f77f6125adbcd0f13a993496ac7f32f
Slupon <sergi.luponsantacana@gmail.com>
2026-01-10 18:36:43 +0100

    More changes

--- src/project_test/evaluate.py
+++ src/project_test/evaluate.py
@@ -1,7 +1,7 @@
 import torch
 import typer
 from data import corrupt_mnist
-from model import MyAwesomeModel
+from project_test.model import MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
